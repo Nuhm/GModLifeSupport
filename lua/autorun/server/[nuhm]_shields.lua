ece4caf127cb198ac106982bf42a0db109f97ed6
Mikey <michael.knox@talktalk.net>
2023-10-15 03:25:19 +0100

    Added /regenall Command
    Commands regens all players on the server to having max shield.
    
    Currently may be a bit buggy and can reset all player shields to 100.

--- lua/autorun/server/[nuhm]_shields.lua
+++ lua/autorun/server/[nuhm]_shields.lua
@@ -52,3 +52,22 @@ hook.Add("PlayerSpawn", "InitializeShieldRegenTimer", function(ply)
         startShieldRegeneration(ply)
     end)
 end)
+
+-- Create a function to restore all players' armor
+local function RegenAllArmor(ply)
+    for _, player in pairs(player.GetAll()) do
+        local maxArmor = player:GetMaxArmor()
+        player:SetArmor(maxArmor)
+        player:PrintMessage(HUD_PRINTTALK, "Your armor has been fully restored by an administrator.")
+    end
+end
+
+-- Register the chat command
+hook.Add("PlayerSay", "RestoreAllArmorCommand", function(ply, text, team)
+    if text:lower() == "/regenall" then
+        if ply:IsSuperAdmin() then
+            RegenAllArmor(ply) -- Call the function to restore armor
+            return ""
+        end
+    end
+end)
