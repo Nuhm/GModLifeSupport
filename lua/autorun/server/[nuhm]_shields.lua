cad88a5a8358389c9bf83deb5378fc93b4674bcc
Mikey <michael.knox@talktalk.net>
2023-10-18 21:28:27 +0100

    Added health regeneration and massive fixes
    - Added /regenallhealth command
    - Change addon name to [nuhm] Life System
    - Fixed how the intervals between regeneration were working, it seems they weren't within the statement checking if it was a valid time to heal or not, meaning players would regen every second no matter the interval value.
    - Added a way for players to regen health.

--- lua/autorun/server/[nuhm]_shields.lua
+++ lua/autorun/server/[nuhm]_shields.lua
@@ -2,23 +2,30 @@
 local debugMode = false;
 
 -- Delays
-local armorRegenDelay = 10 -- Time in seconds before regeneration starts
+local armorRegenDelay = 10  -- Time in seconds before armor regeneration starts
+local healthRegenDelay = 15 -- Time in seconds before health regeneration starts
 
 -- Regen Rate
-local defaultRegenRate = 1
-local playerArmorRegenRate = {} -- Initial amount of armor regenerated per second
-local maxArmorRegenRate = 10    -- Maximum regeneration rate
+local defaultArmorRegenRate = 1
+local defaultHealthRegenRate = 1
+local playerArmorRegenRate = {}  -- Initial amount of armor regenerated per second
+local playerHealthRegenRate = {} -- Initial amount of health regenerated per second
+local maxArmorRegenRate = 10     -- Maximum armor regeneration rate
+local maxHealthRegenRate = 10    -- Maximum health regeneration rate
 
 -- Regen Increments
-local regenRateIncrement = 1             -- Rate at which the regeneration rate increases
-local armorRegenRateIncreaseInterval = 5 -- Time in seconds between rate increases
+local regenRateIncrement = 1              -- Rate at which the regeneration rate increases
+local armorRegenRateIncreaseInterval = 1  -- Time in seconds between armor rate increases
+local healthRegenRateIncreaseInterval = 1 -- Time in seconds between health rate increases
 
 -- Data Stores
 local playerArmorData = {}
-local prevPlayerArmorRegenRate = {};
+local playerHealthData = {}
+local prevPlayerArmorRegenRate = {}
+local prevPlayerHealthRegenRate = {}
 
 if debugMode then
-    print("[nuhm] Shields has been loaded!")
+    print("[nuhm] Life System has been loaded!")
 end
 
 -- Function to start armor regeneration for a player
@@ -29,38 +36,83 @@ local function startArmorRegeneration(ply)
         local maxArmor = playerArmorData[ply] or 100 -- Use the stored max armor value or a default (e.g., 100)
 
         -- Calculate the time since the last regeneration increase
-        local timeSinceLastArmorRegen = CurTime() - ply:GetNWFloat("LastRegenIncreaseTime", 0)
+        local timeSinceLastArmorRegen = CurTime() - ply:GetNWFloat("LastArmorRegenIncreaseTime", 0)
 
         -- If it's time to increase the regeneration rate
         if timeSinceLastArmorRegen >= armorRegenRateIncreaseInterval then
             -- Increase the regeneration rate
+            if prevPlayerArmorRegenRate[ply] == nil then
+                prevPlayerArmorRegenRate[ply] = 0
+            end
+
             playerArmorRegenRate[ply] = math.min(maxArmorRegenRate, prevPlayerArmorRegenRate[ply] + regenRateIncrement)
-            prevPlayerRegenRate[ply] = playerArmorRegenRate[ply]
+            prevPlayerArmorRegenRate[ply] = playerArmorRegenRate[ply]
 
             -- Store the current time for the next increase
-            ply:SetNWFloat("LastRegenIncreaseTime", CurTime())
-        end
+            ply:SetNWFloat("LastArmorRegenIncreaseTime", CurTime())
 
-        local newArmor = math.min(maxArmor, currentArmor + playerArmorRegenRate[ply])
+            local newArmor = math.min(maxArmor, currentArmor + playerArmorRegenRate[ply])
 
-        -- Log a message in the console when a player is being healed
-        if newArmor > currentArmor then
-            if debugMode then
-                print(ply:Nick() ..
-                    " is being regenerated. Armor increased from " ..
-                    currentArmor .. " to " .. newArmor .. " / " .. maxArmor)
+            -- Log a message in the console when a player is being healed
+            if newArmor > currentArmor then
+                if debugMode then
+                    print(ply:Nick() ..
+                        " is having their armor regenerated. Armor increased from " ..
+                        currentArmor .. " to " .. newArmor .. " / " .. maxArmor)
+                end
+                ply:SetArmor(newArmor)
             end
-            ply:SetArmor(newArmor)
+
+
+            -- Store the current regeneration time for incremental increase
+            ply:SetNWFloat("LastArmorRegenTime", CurTime())
         end
+    end
+end
 
+-- Function to start health regeneration for a player
+local function startHealthRegeneration(ply)
+    -- Check if the player hasn't taken damage to their health for the specified time
+    if CurTime() - ply:GetNWFloat("LastHealthDamageTime", 0) >= healthRegenDelay then
+        local currentHealth = ply:Health()
+        local maxHealth = playerHealthData[ply] or 100 -- Use the stored max health value or a default (e.g., 100)
 
-        -- Store the current regeneration time for incremental increase
-        ply:SetNWFloat("LastRegenTime", CurTime())
+        -- Calculate the time since the last health regeneration increase
+        local timeSinceLastHealthRegen = CurTime() - ply:GetNWFloat("LastHealthRegenIncreaseTime", 0)
+
+        -- If it's time to increase the health regeneration rate
+        if timeSinceLastHealthRegen >= healthRegenRateIncreaseInterval then
+            if prevPlayerHealthRegenRate[ply] == nil then
+                prevPlayerHealthRegenRate[ply] = 0
+            end
+
+            -- Increase the health regeneration rate
+            playerHealthRegenRate[ply] = math.min(maxHealthRegenRate, prevPlayerHealthRegenRate[ply] + regenRateIncrement)
+            prevPlayerHealthRegenRate[ply] = playerHealthRegenRate[ply]
+
+            -- Store the current time for the next increase
+            ply:SetNWFloat("LastHealthRegenIncreaseTime", CurTime())
+
+            local newHealth = math.min(maxHealth, currentHealth + playerHealthRegenRate[ply])
+
+            -- Log a message in the console when a player's health is being regenerated
+            if newHealth > currentHealth then
+                if debugMode then
+                    print(ply:Nick() ..
+                        " is having their health regenerated. Health increased from " ..
+                        currentHealth .. " to " .. newHealth .. " / " .. maxHealth)
+                end
+                ply:SetHealth(newHealth)
+            end
+
+            -- Store the current health regeneration time for incremental increase
+            ply:SetNWFloat("LastHealthRegenTime", CurTime())
+        end
     end
 end
 
 -- Create a function to restore all players' armor
-local function RegenAllArmor(ply)
+local function RegenAllArmor()
     for _, player in pairs(player.GetAll()) do
         local maxArmor = playerArmorData[player] or 100 -- Use the stored max armor value or a default (e.g., 100)
         player:SetArmor(maxArmor)
@@ -68,8 +120,17 @@ local function RegenAllArmor(ply)
     end
 end
 
+-- Create a function to restore all players' health
+local function RegenAllHealth()
+    for _, player in pairs(player.GetAll()) do
+        local maxHealth = playerHealthData[player] or 100 -- Use the stored max health value or a default (e.g., 100)
+        player:SetHealth(maxHealth)
+        player:PrintMessage(HUD_PRINTTALK, "Your health has been fully restored by an administrator.")
+    end
+end
+
 if debugMode then
-    print("[nuhm] Shields commands have been loaded!")
+    print("[nuhm] Life System commands have been loaded!")
 end
 
 -- Function to set up hooks
@@ -84,26 +145,44 @@ local function SetupHooks()
                     armorLevel = 100 -- Set it to 100 if it's less than 100
                 end
                 playerArmorData[ply] = armorLevel
+
+                local healthLevel = ply:Health()
+                if healthLevel < 100 then
+                    healthLevel = 100 -- Set it to 100 if it's less than 100
+                end
+                playerHealthData[ply] = healthLevel
             end
         end)
     end)
 
     -- Hook to track player damage
     hook.Add("PlayerHurt", "TrackLastDamageTime", function(ply)
-        ply:SetNWFloat("LastDamageTime", CurTime())
-        prevPlayerRegenRate[ply] = defaultRegenRate
+        -- Check if the damage is to their health
+        if ply:Health() < ply:GetMaxHealth() then
+            ply:SetNWFloat("LastHealthDamageTime", CurTime())
+            prevPlayerHealthRegenRate[ply] = defaultHealthRegenRate
+        end
+        -- Check if the damage is to their armor
+        if ply:Armor() < 100 then
+            ply:SetNWFloat("LastArmorDamageTime", CurTime())
+            prevPlayerArmorRegenRate[ply] = defaultArmorRegenRate
+        end
     end)
 
     -- Hook to start regeneration on player spawn
-    hook.Add("PlayerSpawn", "InitializeArmorRegenTimer", function(ply)
+    hook.Add("PlayerSpawn", "InitializeRegenTimers", function(ply)
         timer.Create("ArmorRegenTimer_" .. ply:EntIndex(), 1, 0, function()
             startArmorRegeneration(ply)
         end)
+
+        timer.Create("HealthRegenTimer_" .. ply:EntIndex(), 1, 0, function()
+            startHealthRegeneration(ply)
+        end)
     end)
 
-    -- Register the chat command
+    -- Register the chat command to restore armor
     hook.Add("PlayerSay", "RestoreAllArmorCommand", function(ply, text, team)
-        if text:lower() == "/regenall" then
+        if text:lower() == "/regenallarmor" then
             if ply:IsSuperAdmin() then
                 RegenAllArmor(ply) -- Call the function to restore armor
                 return ""
@@ -113,8 +192,20 @@ local function SetupHooks()
         end
     end)
 
+    -- Register the chat command to restore health
+    hook.Add("PlayerSay", "RestoreAllHealthCommand", function(ply, text, team)
+        if text:lower() == "/regenallhealth" then
+            if ply:IsSuperAdmin() then
+                RegenAllHealth(ply) -- Call the function to restore health
+                return ""
+            else
+                return ""
+            end
+        end
+    end)
+
     if debugMode then
-        print("[nuhm] Shields hooks have been added!")
+        print("[nuhm] Life System hooks have been added!")
     end
 end
 
